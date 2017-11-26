f906347ec3677c4d880b98933a5616fb14cc3cfa
Blue-Sky-r <robert.puskajler@yahoo.ca>
2017-11-26 00:02:04 -0500

    task 03b

--- 2015/03/u03.py
+++ 2015/03/u03.py
@@ -10,41 +10,49 @@ verbose = 0
 
 class Position:
 
-    def __init__(self):
+    def __init__(self, members=1):
         self.visited = []
-        self.visit(0,0)
+        self.pos = [(0,0) for i in range(members+1)]
+        for i in range(members):
+            self.visit((0,0), i)
 	  
-    def visit(self, x, y):
-        self.x = x
-        self.y = y
-        if (x,y) in self.visited:
-            if verbose: print "(%d,%d) already visited" % (x,y)
+    def visit(self, xy, member):
+        self.pos[member] = xy
+        if xy in self.visited:
+            if verbose: print "(%d,%d) already visited" % (xy[0],xy[1])
             return
-        self.visited.append((x,y))
+        self.visited.append(xy)
         if verbose: print self.visited
                     
     def count_visited(self):
         return len(self.visited)
           
-    def move_north(self):
-        return self.visit(self.x, self.y+1)
+    def move_north(self, member):
+        x,y = self.pos[member]
+        return self.visit((x,y+1), member)
 
-    def move_south(self):
-        return self.visit(self.x, self.y-1)
+    def move_south(self, member):
+        x,y = self.pos[member]
+        return self.visit((x,y-1), member)
 
-    def move_east(self):
-        return self.visit(self.x+1, self.y)
+    def move_east(self, member):
+        x,y = self.pos[member]
+        return self.visit((x+1,y), member)
 
-    def move_west(self):
-        return self.visit(self.x-1, self.y)
+    def move_west(self, member):
+        x,y = self.pos[member]
+        return self.visit((x-1,y), member)
 
-    def instructions(self, s):
+    def instructions(self, s, members=1):
+        member = 0
         for step in s:
-            if verbose: print "step=",step
-            if step == '^': self.move_north()
-            if step == 'v': self.move_south()
-            if step == '>': self.move_east()
-            if step == '<': self.move_west()
+            if verbose: print "step=",step,", member=",member
+            if step == '^': self.move_north(member)
+            if step == 'v': self.move_south(member)
+            if step == '>': self.move_east(member)
+            if step == '<': self.move_west(member)
+            member += 1
+            if member>members-1: member=0
 
 
 def testcase(input, result, task_b=False):
@@ -52,7 +60,7 @@ def testcase(input, result, task_b=False):
     print "TestCase",'B' if task_b else 'A',
     print "for input:",input,"\t expected result:",result,
     p = Position()
-    p.instructions(input)
+    p.instructions(input, 2 if task_b else 1)
     r = p.count_visited()
     print 'got:',r,'\t','OK' if r == result else 'ERR'
     print
@@ -75,5 +83,23 @@ with open(data) as f:
         p.instructions(line)
 # 2565
 print 'Task A input file:',data,'Result:',p.count_visited()
+print
+
+# ========
+#  Task B
+# ========
+
+testcase('^>',          3, task_b=True)
+testcase('^>v<',        3, task_b=True)
+testcase('^v^v^v^v^v', 11, task_b=True)
+
+p = Position()
+with open(data) as f:
+    for line in f:
+        if not line: continue
+        p.instructions(line, members=2)
+
+# 2639
+print 'Task B input file:',data,'Result:',p.count_visited()
 print
 
