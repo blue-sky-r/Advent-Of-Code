01ae0d6c56e87f6ad637a03d6750db4cfecac1bb
Blue-Sky-r <robert.puskajler@yahoo.ca>
2019-12-04 12:51:39 +0100

    unit 04 - task B

--- 2019/04/u04.py
+++ 2019/04/u04.py
@@ -20,20 +20,26 @@ class Password:
         l, h = l_h.split('-')
         self.lh = int(l), int(h)
 
-    def validate(self, psw):
+    def validate_a(self, psw):
         """ validate password psw """
         # return self.is_Xdigit(psw) and self.has_double(psw) and self.is_inc(psw)
         return self.has_double(psw) and self.is_inc(psw)
+
+    def validate_b(self, psw):
+        """ validate password psw """
+        # return self.is_Xdigit(psw) and self.has_double(psw) and self.is_inc(psw)
+        return self.has_double(psw, exact=2) and self.is_inc(psw)
 
     def is_Xdigit(self, psw, x=6):
         """ It is a six-digit number """
         return len(psw) == x
 
-    def has_double(self, psw):
+    def has_double(self, psw, exact=None):
         """ Two adjacent digits are the same (like 22 in 122345) """
         for d in range(10):
             cnt = psw.count('%d' % d)
-            if cnt > 1: return True
+            if not exact and cnt > 1: return True
+            if exact and cnt == exact: return True
         return False
 
     def is_inc(self, psw):
@@ -44,31 +50,32 @@ class Password:
             left = int(d)
         return True
 
-    def all_valid_psw(self):
+    def all_psw(self):
         """ generate all valid passwords within range """
         for num in range(self.lh[0], self.lh[1]+1):
-            psw = "%d" % num
-            if not self.validate(psw): continue
-            if verbose: print 'DBG: psw:', psw,'OK'
-            yield psw
+            yield "%d" % num
 
     def task_a(self, input):
         """ task A """
         if self.lh:
             cnt = 0
-            for psw in self.all_valid_psw():
-                cnt += 1
+            for psw in self.all_psw():
+                if self.validate_a(psw):
+                    if verbose: print 'DBG: psw:', psw, 'OK'
+                    cnt += 1
             return cnt
-        return self.validate(input)
+        return self.validate_a(input)
 
     def task_b(self, input):
         """ task B """
         if self.lh:
             cnt = 0
-            for psw in self.all_valid_psw():
-                cnt += 1
+            for psw in self.all_psw():
+                if self.validate_b(psw):
+                    if verbose: print 'DBG: psw:', psw, 'OK'
+                    cnt += 1
             return cnt
-        return self.validate(input)
+        return self.validate_b(input)
 
 
 def testcase(sut, input, result, task_b=False):
@@ -101,3 +108,11 @@ testcase(Password(), input, 910)
 # ========
 #  Task B
 # ========
+
+# test cases
+testcase(Password(), '112233', True,    task_b=True)
+testcase(Password(), '123444', False,   task_b=True)
+testcase(Password(), '111122', True,    task_b=True)
+
+# 598
+testcase(Password(), input, 598, task_b=True)
