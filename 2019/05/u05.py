d8a6620e8f06d31ce55ab2354d7c8e1d0e2480c0
Blue-Sky-r <robert.puskajler@yahoo.ca>
2019-12-05 20:08:29 +0100

    unit 05 - task B (work in progress)

--- 2019/05/u05.py
+++ 2019/05/u05.py
@@ -17,6 +17,10 @@ class Computer2:
              2: self.asm_mult,
              3: self.asm_input,
              4: self.asm_output,
+             5: self.asm_jmp_if_true,
+             6: self.asm_jmp_if_false,
+             7: self.asm_less_than,
+             9: self.asm_equal,
             99: self.asm_halt
         }
         self.input  = input
@@ -24,7 +28,7 @@ class Computer2:
 
     def asm_halt(self, code):
         """ halt the computer """
-        print 'HALT @',self.pc
+        if verbose: print 'HALT @',self.pc
         self.running = False
         return self
 
@@ -32,7 +36,7 @@ class Computer2:
         """ add A + B -> C """
         ma, mb, mc = self.decode_parameter_mode(code, 1), self.decode_parameter_mode(code,2), self.decode_parameter_mode(code, 3)
         assert mc == 0, 'par.c has to have indirect(0) mode !'
-        self.program[self.get_parameter(3, 1)] = self.get_parameter(1, ma) + self.get_parameter(2, mb)
+        self.program[self.get_parameter(3)] = self.get_parameter(1, ma) + self.get_parameter(2, mb)
         self.pc += 4
         return self
 
@@ -40,7 +44,7 @@ class Computer2:
         """ mul A * B -> C """
         ma, mb, mc = self.decode_parameter_mode(code,1), self.decode_parameter_mode(code, 2), self.decode_parameter_mode(code, 3)
         assert mc == 0, 'par.c has to have indirect(0) mode !'
-        self.program[self.get_parameter(3, 1)] = self.get_parameter(1, ma) * self.get_parameter(2, mb)
+        self.program[self.get_parameter(3)] = self.get_parameter(1, ma) * self.get_parameter(2, mb)
         self.pc += 4
         return self
 
@@ -48,10 +52,10 @@ class Computer2:
         """ takes a single integer as input and saves it to the address given by its only parameter """
         mode = self.decode_parameter_mode(code, par=1)
         assert mode == 0, 'INPUT par mode has to be indirect(0) !'
-        par = self.get_parameter(1, 1)
+        par = self.get_parameter(1)
         # get input from queue
         data = self.input.pop(0)
-        print 'IN:',data
+        if verbose: print 'IN:',data
         # store data
         self.program[par] = data
         self.pc += 2
@@ -63,8 +67,36 @@ class Computer2:
         #assert mode == 0, 'OUPUT par mode has to be indirect(0) !'
         data = self.get_parameter(1, mode)
         self.output.append(data)
-        print 'OUT:', data
+        if verbose: print 'OUT:', data
         self.pc += 2
+        return self
+
+    def asm_jmp_if_true(self, code):
+        """ if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter """
+        ma, mb = self.decode_parameter_mode(code, 1), self.decode_parameter_mode(code, 2)
+        self.pc = mb if ma != 0 else self.pc + 3
+        return self
+
+    def asm_jmp_if_false(self, code):
+        """ if the first parameter is zero, it sets the instruction pointer to the value from the second parameter """
+        ma, mb = self.decode_parameter_mode(code, 1), self.decode_parameter_mode(code, 2)
+        self.pc = mb if ma == 0 else self.pc + 3
+        return self
+
+    def asm_less_than(self, code):
+        """ if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter.
+        Otherwise, it stores 0 """
+        ma, mb = self.decode_parameter_mode(code, 1), self.decode_parameter_mode(code, 2)
+        value = 1 if ma < mb else 0
+        self.program[self.get_parameter(3)] = value
+        return self
+
+    def asm_equal(self, code):
+        """ if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter.
+        Otherwise, it stores 0 """
+        ma, mb = self.decode_parameter_mode(code, 1), self.decode_parameter_mode(code, 2)
+        value = 1 if ma == mb else 0
+        self.program[self.get_parameter(3)] = value
         return self
 
     def decode_parameter_mode(self, code, par):
@@ -75,7 +107,7 @@ class Computer2:
         """ return instruction code """
         return code % 100
 
-    def get_parameter(self, par, mode):
+    def get_parameter(self, par, mode=1):
         """ get parameter par=1,2,3 mode 0=indirect, 1=immediate """
         par = self.program[self.pc + par]
         assert mode in [0,1], 'Parameter mode has to be 0,1 only !'
@@ -94,12 +126,6 @@ class Computer2:
         asm_fnc = self.asm[self.decode_insctruction_code(code)]
         return asm_fnc(code)
 
-    def program_alarm(self, codestr):
-        """ restore gravity assist """
-        at1, at2 = codestr[-4:-2], codestr[-2:]
-        self.program[1], self.program[2] = int(at1), int(at2)
-        return self
-
     def program_load(self, prog):
         """ load csv string format to list """
         self.program = [int(i) for i in prog.split(',')]
@@ -107,18 +133,17 @@ class Computer2:
         self.running = False
         return self
 
-    def run_prog(self, prog, alarm=None):
+    def run_prog(self, prog):
         """ execute program prog """
         self.program_load(prog)
-        if alarm: self.program_alarm(alarm)
         self.running = True
         while self.running:
             self.step()
 
-    def task_a(self, input, alarm=None):
+    def task_a(self, input):
         """ task A """
-        self.run_prog(input, alarm)
-        return self.program[0]
+        self.run_prog(input)
+        return self.output.pop() if self.output else None
 
     def task_b(self, input, find):
         """ task B """
@@ -141,7 +166,7 @@ def testcase(sut, input, result, alarm=None, task_b=False):
     print "TestCase", "B" if task_b else "A",
     print "for input:", data if 'data' in vars() else input,
     print "\t expected result:", result,
-    r = sut.task_a(input, alarm) if not task_b else sut.task_b(input, find=alarm)
+    r = sut.task_a(input) if not task_b else sut.task_b(input)
     print 'got:',r,'\t','[ OK ]' if r == result else '[ ERR ]'
     if verbose > 1: print 'Dump:',sut.program
     print
@@ -151,8 +176,8 @@ def testcase(sut, input, result, alarm=None, task_b=False):
 # ========
 
 # test cases
-testcase(Computer2(), '1002,4,3,4,33', 1002)
-xxx
+testcase(Computer2(), '1002,4,3,4,33', None)
+
 # 5182797
 testcase(Computer2(),  None, 5182797, None)
 
