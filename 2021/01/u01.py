fb1e35f85ac9b209beeb2befc9df6593b4b9a986
blue-sky-r <robert.puskajler@yahoo.ca>
2021-12-03 00:38:00 +0100

    unit 01 - task B (Task A refactored to be more pythonic)

--- 2021/01/u01.py
+++ 2021/01/u01.py
@@ -10,8 +10,6 @@ __url__ = 'http://adventofcode.com/%s/day/%s' % (__year__, __day__)
 
 verbose = 0
 
-from functools import reduce
-
 
 class Sonar:
 
@@ -22,13 +20,11 @@ class Sonar:
         self.measurements = depth_list
 
     def calc_deltas(self):
-        """ count deltas """
-        self.delta = []
-        iterator = iter(self.measurements)
-        last = next(iterator)
-        for d in iterator:
-            self.delta.append(d - last)
-            last = d
+        self.delta = [ b - a for a,b in zip(self.measurements[:-1], self.measurements[1:]) ]
+
+    def calc_3sumdeltas(self):
+        self.sums  = [ a+b+c for a,b,c in zip(self.measurements[:-2], self.measurements[1:], self.measurements[2:]) ]
+        self.delta = [ b - a for a,b in zip(self.sums[:-1], self.sums[1:]) ]
 
     def count_pos(self):
         return len([ d for d in self.delta if d > 0 ])
@@ -44,7 +40,9 @@ class Sonar:
 
     def task_b(self, input):
         """ task B """
-        return None
+        self.add_measurements(input)
+        self.calc_3sumdeltas()
+        return self.count_pos()
 
 
 def testcase_a(sut, input, result):
@@ -61,6 +59,29 @@ def testcase_a(sut, input, result):
     print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
     print()
 
+def testcase_b(sut, input, result):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ int(line.strip()) for line in f ]
+    #
+    print("TestCase B using input:", data if 'data' in vars() else input)
+    print("\t expected result:", result)
+    r = sut.task_b(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+
+# ======
+#  MAIN
+# ======
+
+print()
+print(__motd__, __url__)
+print()
+
 # ========
 #  Task A
 # ========
@@ -70,3 +91,13 @@ testcase_a(Sonar(), [199, 200, 208, 210, 200, 207, 240, 269, 260, 263],  7)
 
 # 1564
 testcase_a(Sonar(),   None,     1564)
+
+# ========
+#  Task B
+# ========
+
+# test cases
+testcase_b(Sonar(), [199, 200, 208, 210, 200, 207, 240, 269, 260, 263],  5)
+
+# 1611
+testcase_b(Sonar(),   None,     1611)
