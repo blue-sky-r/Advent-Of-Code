39f3c062640383b1e544b86205072b3707ee3278
blue-sky-r <robert.puskajler@yahoo.ca>
2021-12-07 19:21:07 +0100

    unit 05 - task AB

--- 2021/05/u05.py
+++ 2021/05/u05.py
@@ -18,9 +18,9 @@ class HydroMap:
         self.size = size
 
     def print(self, zero='.'):
-        print()
         for row in range(self.size):
             print(''.join([ self.chr_xy((col,row))  for col in range(self.size) ]))
+        print()
 
     def chr_xy(self, xy:tuple, zero='.'):
         c = self.data.get(xy, 0)
@@ -33,27 +33,33 @@ class HydroMap:
     def add_xy_val(self, xy: tuple, val:int):
         self.data[xy] = self.get_xy(xy) + val
 
-    def add_str_line(self, line: str, sep=' -> '):
-        """ x1,y1 -> x2,y2 """
+    def add_str_line(self, line: str, hvonly=True, sep=' -> '):
+        """ x1,y1 -> x2,y2 H/V only - limit to Horizontal / Vertical lines"""
         x1y1, _, x2y2 = line.partition(sep)
-        x1,y1 = x1y1.split(',')
-        x2,y2 = x2y2.split(',')
-        return self.add_hv_line((int(x1), int(y1)), (int(x2), int(y2)))
+        # startpoint
+        x1,y1 = map(int, x1y1.split(','))
+        # endpoint
+        x2,y2 = map(int, x2y2.split(','))
+        # str -> int
+        return self.add_line((x1,y1), (x2,y2), hvonly)
 
-    def add_hv_line(self, xy1:tuple, xy2:tuple, val=1):
+    def add_line(self, xy1:tuple, xy2:tuple, hvonly=True, val=1):
         deltax, deltay = xy2[0] - xy1[0], xy2[1] - xy1[1]
-        # exit if not H/ or V line
-        if deltax != 0 and deltay != 0:
+        # exit if H/V required and not H or V line
+        if hvonly and deltax != 0 and deltay != 0:
             return False
+        # start from startpoint
         x, y = xy1[0], xy1[1]
         while True:
             self.add_xy_val((x,y), val)
+            # did ew reachj endpoint ?
             if x == xy2[0] and y == xy2[1]:
                 break
+            # move by sign(delta) = delta//abs(delta)
             if deltax:
-                x += 1 if deltax > 0 else -1
+                x += deltax // abs(deltax)
             if deltay:
-                y += 1 if deltay > 0 else -1
+                y += deltay // abs(deltay)
         return True
 
 
@@ -67,21 +73,22 @@ class Hydro:
         selected = [ self.hmap.get_xy(xy) for xy in self.hmap.data if self.hmap.get_xy(xy) >= threshold ]
         return len(selected)
 
-    def process_input(self, input: list):
+    def process_input(self, input: list, hvonly=False):
         for line in input:
-            r = self.hmap.add_str_line(line)
+            r = self.hmap.add_str_line(line, hvonly)
             if verbose:
                 print(line, 'ok' if r else 'ignored')
                 self.hmap.print()
 
     def task_a(self, input: list):
-        """ task A """
-        self.process_input(input)
+        """ task A - Horizontal / Vertial lines only"""
+        self.process_input(input, hvonly=True)
         return self.count_threshold()
 
     def task_b(self, input):
         """ task B """
-        return None
+        self.process_input(input, hvonly=False)
+        return self.count_threshold()
 
 
 def testcase_a(sut, input, result):
@@ -158,3 +165,12 @@ testcase_a(Hydro(), testdata,  5)
 # 4421
 testcase_a(Hydro(),   None,   4421)
 
+# ========
+#  Task B
+# ========
+
+# test cases
+testcase_b(Hydro(), testdata,  12)
+
+# 18674
+testcase_b(Hydro(),   None,    18674)
