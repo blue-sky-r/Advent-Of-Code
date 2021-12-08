c0b27e7219b2793be63f36a8c1506eac32d0031c
blue-sky-r <robert.puskajler@yahoo.ca>
2021-12-08 22:36:11 +0100

    unit 06 - task AB

--- 2021/06/u06.py
+++ 2021/06/u06.py
@@ -14,32 +14,42 @@ verbose = 0
 class Lanternfish:
 
     def __init__(self):
-        self.population = []
+        self.population = {}
         self.reset_d = 6
         self.new_d   = 8
-        self.spawn_d = -1
 
     def print(self, txt: str):
-        print(txt, ','.join([ '%s' % f for f in self.population ]))
+        print(txt, ''.join([ ('%s,' % day)*cnt for day,cnt in self.population.items() ])[:-1],'\t pop=',self.count_population())
+
+    def init_population(self, line: str):
+        """ init by CSV string """
+        for f in line.split(','):
+            day = int(f)
+            self.population[day] = self.population.get(day, 0) + 1
 
     def next_day(self):
-        # dec timer
-        population = [ f-1 for f in self.population ]
-        # count expired
-        spawn_num = population.count(self.spawn_d)
-        # reset expired
-        population = [ self.reset_d if f == self.spawn_d else f for f in population ]
-        # spawn new
-        self.population = population + [ self.new_d ] * spawn_num
+        new_population = {}
+        for day,cnt in self.population.items():
+            new_day = day - 1
+            if new_day >= 0:
+                new_population[new_day] = new_population.get(new_day, 0) + cnt
+            else:
+                new_population[self.reset_d] = new_population.get(self.reset_d, 0) + cnt
+                new_population[self.new_d]   = new_population.get(self.new_d, 0)   + cnt
+        self.population = new_population
+
+    def count_population(self):
+        cnt = sum(self.population.values())
+        return cnt
 
     def task_a(self, input: list, days=80):
         """ task A """
-        self.population = [int(i) for i in input[0].split(',')]
+        self.init_population(input[0])
         if verbose: self.print('=init:')
         for d in range(1, days+1):
             self.next_day()
             if verbose: self.print('+%3dd:' % d)
-        return len(self.population)
+        return self.count_population()
 
     def task_b(self, input: list, days=256):
         """ task B """
@@ -109,3 +119,12 @@ testcase_a(Lanternfish(), testdata,  5934)
 # 351188
 testcase_a(Lanternfish(),   None,     351188)
 
+# ========
+#  Task B
+# ========
+
+# test cases
+testcase_b(Lanternfish(), testdata,    26984457539)
+
+# 1595779846729
+testcase_b(Lanternfish(),   None,    1595779846729)
