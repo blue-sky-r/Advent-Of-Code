1d5a2e88d961ef5783ccef6a606eda77d27ccabb
blue-sky-r <robert.puskajler@yahoo.ca>
2021-12-10 11:03:56 +0100

    unit 08 - task A

---
+++ 2021/08/u08.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+__day__  = 8
+
+__year__ = 2021
+
+__motd__ = '--- Year %s -- Day %s ---' % (__year__, __day__)
+
+__url__ = 'http://adventofcode.com/%s/day/%s' % (__year__, __day__)
+
+verbose = 0
+
+
+class Segments:
+
+    def __init__(self):
+        self.signal = []
+        self.digits = []
+        self.val2segcnt = {
+            0: 6,
+            1: 2,
+            2: 5,
+            3: 5,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 3,
+            8: 7,
+            9: 6
+        }
+
+    def process_input(self, input: list):
+        for line in input:
+            signal, value = line.split(' | ')
+            self.signal.append(signal.split())
+            self.digits.append(value.split())
+
+    def count_digits_with_num_segments(self, num: int):
+        r = [ digit for value in self.digits for digit in value if len(digit) == num ]
+        return len(r)
+
+    def count_digits(self, digits: list):
+        return sum([self.count_digits_with_num_segments(self.val2segcnt[d]) for d in digits])
+
+    def task_a(self, input: list):
+        """ task A """
+        self.process_input(input)
+        return self.count_digits([1, 4, 7, 8])
+
+    def task_b(self, input: list):
+        """ task B """
+        return None
+
+
+def testcase_a(sut, input, result):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ line.strip() for line in f ]
+    #
+    print("TestCase A using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ line.strip() for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_a(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+def testcase_b(sut, input, result):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ line.strip() for line in f ]
+    #
+    print("TestCase B using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ line.strip() for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_b(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+
+# ======
+#  MAIN
+# ======
+
+print()
+print(__motd__, __url__)
+print()
+
+testdata = """
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce   
+"""
+
+# ========
+#  Task A
+# ========
+
+# test cases
+testcase_a(Segments(), testdata,  26)
+
+# 365
+testcase_a(Segments(),   None,    365)
+
