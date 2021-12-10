12d8e1319e316b4253539aafd87d0c369aab9862
blue-sky-r <robert.puskajler@yahoo.ca>
2021-12-10 11:31:18 +0100

    unit 08 - task B

--- 2021/08/u08.py
+++ 2021/08/u08.py
@@ -11,36 +11,110 @@ __url__ = 'http://adventofcode.com/%s/day/%s' % (__year__, __day__)
 verbose = 0
 
 
+class Digit:
+
+    def __init__(self):
+        pass
+
+
+
+
 class Segments:
 
     def __init__(self):
         self.signal = []
         self.digits = []
-        self.val2segcnt = {
-            0: 6,
-            1: 2,
-            2: 5,
-            3: 5,
-            4: 4,
-            5: 5,
-            6: 6,
-            7: 3,
-            8: 7,
-            9: 6
+        # value -> sorted on segments
+        self.val2segments = {
+            0: 'abcefg',
+            1: 'cf',
+            2: 'acdeg',
+            3: 'acdfg',
+            4: 'bcdf',
+            5: 'abdfg',
+            6: 'abdefg',
+            7: 'acf',
+            8: 'abcdefg',
+            9: 'abcdfg'
         }
+
+    def segments_to_values(self):
+        """ segment -> [values] """
+        d = {}
+        for segment in 'abcdefg':
+            d[segment] = [ val for val,segments in self.val2segments.items() if segment in segments ]
+        return d
 
     def process_input(self, input: list):
         for line in input:
             signal, value = line.split(' | ')
-            self.signal.append(signal.split())
-            self.digits.append(value.split())
+            self.signal.append([''.join(sorted(s)) for s in signal.split()])
+            self.digits.append([''.join(sorted(v)) for v in value.split()])
 
     def count_digits_with_num_segments(self, num: int):
         r = [ digit for value in self.digits for digit in value if len(digit) == num ]
         return len(r)
 
     def count_digits(self, digits: list):
-        return sum([self.count_digits_with_num_segments(self.val2segcnt[d]) for d in digits])
+        return sum([self.count_digits_with_num_segments(len(self.val2segments[d])) for d in digits])
+
+    def solve_by_segments_on(self, signal: list):
+        """ [acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab] ->
+            {abcdefg:[8] bcdef:[2,3,5] acdfg:[2,3,5] abcdf:[2,3,5] abd:[7] abcdef:[0,6,9] bcdefg:[0,6,9] abef:[4] abcdeg:[0,6,9] ab:[1]} """
+        wiring = {}
+        for s in signal:
+            wiring[s] = [ val for val,segments in self.val2segments.items() if len(s) == len(segments) ]
+        return wiring
+
+    def segment_to_wire(self, signal, connected):
+        """ find segment connection to wire """
+        # { a: [0,2,3,5,6,7,8,9] ... }
+        for segment, numbers in self.segments_to_values().items():
+            # ship already connected
+            if segment in connected:
+                continue
+            # possible variants (all - already connected)
+            wireto = set('abcdefg') - set(connected.values())
+            # {abcdefg:[8] bcdef:[2,3,5] acdfg:[2,3,5] abcdf:[2,3,5] abd:[7] abcdef:[0,6,9] bcdefg:[0,6,9] abef:[4] abcdeg:[0,6,9] ab:[1]}
+            for wiredsegs, wiredvals in self.solve_by_segments_on(signal).items():
+                if set(numbers) >= set(wiredvals):
+                    wireto = wireto & set(wiredsegs)
+                    if len(wireto) == 1:
+                        return segment, wireto
+                    if len(wireto) == 0:
+                        break
+        return None
+
+    def wiring_solver(self, signal: list):
+        """ [acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab] ->
+            {ab:1 dab:7 eafb:4 acedgfb:8 cdfbe:5 gcdfa:2 fbcad:3 cefabd:9 cdfgeb:6 cagedb:0} """
+        # segment -> wire_label
+        connected = {}
+        # repeat until all segments are connected
+        while len(connected) < 7:
+            segment, wireto = self.segment_to_wire(signal, connected)
+            if wireto and len(wireto) == 1:
+                connected[segment] = list(wireto)[0]
+        #
+        return connected
+
+    def decode_value(self, wiring: dict, digits: list):
+        """ {segment: wire} [cdfeb fcadb cdfeb cdbaf] -> 5353 """
+        # build new segment wiring
+        wire2val = {}
+        for val,seg in self.val2segments.items():
+            wires = ''.join(sorted([ wiring[s] for s in seg ]))
+            wire2val[wires] = str(val)
+        #
+        return int(''.join([ wire2val[d] for d in digits ]))
+
+    def values_solver(self):
+        values = []
+        for signal, digits in zip(self.signal, self.digits):
+            wiring = self.wiring_solver(signal)
+            value = self.decode_value(wiring, digits)
+            values.append(value)
+        return values
 
     def task_a(self, input: list):
         """ task A """
@@ -49,7 +123,9 @@ class Segments:
 
     def task_b(self, input: list):
         """ task B """
-        return None
+        self.process_input(input)
+        values = self.values_solver()
+        return sum(values)
 
 
 def testcase_a(sut, input, result):
@@ -62,7 +138,7 @@ def testcase_a(sut, input, result):
     #
     print("TestCase A using input:", data if 'data' in vars() else input)
     # read multiline string as input
-    if input.count('\n') > 2:
+    if input.count('\n'):
         input = [ line.strip() for line in input.splitlines() ]
         # optional delete the first empty line
         if len(input[0]) == 0:
@@ -83,7 +159,7 @@ def testcase_b(sut, input, result):
     #
     print("TestCase B using input:", data if 'data' in vars() else input)
     # read multiline string as input
-    if input.count('\n') > 2:
+    if input.count('\n'):
         input = [ line.strip() for line in input.splitlines() ]
         # optional delete the first empty line
         if len(input[0]) == 0:
@@ -126,3 +202,19 @@ testcase_a(Segments(), testdata,  26)
 # 365
 testcase_a(Segments(),   None,    365)
 
+# ========
+#  Task B
+# ========
+
+testdata2 = """
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+"""
+
+# test cases
+testcase_b(Segments(), testdata2, 5353)
+
+# test cases
+testcase_b(Segments(), testdata, 61229)
+
+# 975706
+testcase_b(Segments(),   None,    975706)
