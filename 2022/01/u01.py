eb3e1a613ff2c31accb0d8918974f5bbed490512
blue-sky-r <robert.puskajler@yahoo.ca>
2022-12-04 10:07:26 +0100

    unit 01 - task A

---
+++ 2022/01/u01.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+__day__  = 1
+
+__year__ = 2022
+
+__motd__ = '--- Year %s -- Day %s ---' % (__year__, __day__)
+
+__url__ = 'http://adventofcode.com/%s/day/%s' % (__year__, __day__)
+
+verbose = 0
+
+
+class Calories:
+
+    def __init__(self):
+        pass
+
+    def aggregate_calories(self, input):
+        """ sum of calories per Elve """
+        elve, calories, inventory = 1, [], {}
+        for line in input:
+            if not line:
+                inventory[elve] = sum(calories)
+                elve, calories = elve+1, []
+                continue
+            calories.append(int(line))
+        inventory[elve] = sum(calories)
+        return inventory
+
+    def max_calories(self, calories: dict):
+        """ return max calories """
+        max_key = max(calories, key=lambda k: calories[k])
+        return calories[max_key]
+
+    def task_a(self, input: list):
+        """ task A """
+        inventory = self.aggregate_calories(input)
+        return self.max_calories(inventory)
+
+    def task_b(self, input: list):
+        """ task B """
+        return None
+
+
+def testcase_a(sut, input, result):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ line.strip() for line in f ]
+    #
+    print("TestCase A using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ line.strip() for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_a(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+def testcase_b(sut, input, result):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ line.strip() for line in f ]
+    #
+    print("TestCase B using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ line.strip() for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_b(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+
+# ======
+#  MAIN
+# ======
+
+print()
+print(__motd__, __url__)
+print()
+
+testdata = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+# ========
+#  Task A
+# ========
+
+# test cases
+testcase_a(Calories(), testdata,  24000)
+
+# 70509
+testcase_a(Calories(),   None,    70509)
+
+# ========
+#  Task B
+# ========
+
+# test cases
+#testcase_b(C(), testdata,  2)
+
+# 2
+#testcase_b(C(),   None,    2)
