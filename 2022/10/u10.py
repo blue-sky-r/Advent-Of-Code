884f0109e1abdc525df862b71118a39ea3f2f2bf
blue-sky-r <robert.puskajler@yahoo.ca>
2022-12-13 21:27:55 +0100

    unit 10 - task A

---
+++ 2022/10/u10.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+__day__  = 10
+
+__year__ = 2022
+
+__motd__ = '--- Year %s -- Day %s ---' % (__year__, __day__)
+
+__url__ = 'http://adventofcode.com/%s/day/%s' % (__year__, __day__)
+
+verbose = 0
+
+
+class CPU:
+
+    def __init__(self):
+        self.X = 1
+        self.CLK = 1
+        self.signalstrength = 0
+        self.ssupdate = {
+            'ofs': -20,
+            'each': 40
+        }
+        self.ins_set = {
+            'noop': self._noop,
+            'addx': self._addx
+        }
+
+    def _noop(self, v=None):
+        self.CLK += 1
+        self.update_signalstrength()
+
+    def _addx(self, v):
+        self.CLK += 1
+        self.update_signalstrength()
+        self.CLK += 1
+        self.X += v
+        self.update_signalstrength()
+
+    def update_signalstrength(self):
+        """ update 20th + each 40th clk cycle """
+        update_cycle = (self.CLK + self.ssupdate['ofs']) % self.ssupdate['each']
+        if update_cycle == 0:
+            self.signalstrength += self.CLK * self.X
+            if verbose: print('update_ss() CLK=',self.CLK, 'X=',self.X, 'ss=',self.signalstrength)
+
+    def exec(self, instruction):
+        """  """
+        ins, par = instruction, None
+        has_par = ' ' in instruction
+        if has_par:
+            ins, par = instruction.split()
+            par = int(par)
+        fnc = self.ins_set.get(ins)
+        fnc(par)
+
+    def task_a(self, input: list):
+        """ task A """
+        for instruction in input:
+            self.exec(instruction)
+        return self.signalstrength
+
+    def task_b(self, input: list):
+        """ task B """
+        return None
+
+
+def testcase_a(sut, input, result, trim=str.rstrip):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ trim(line) for line in f ]
+            # file is single line only
+            if len(input) == 1:
+                input = input[0]
+    #
+    print("TestCase A using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ trim(line) for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_a(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+def testcase_b(sut, input, result, trim=str.rstrip):
+    """ testcase verifies if input returns result """
+    # read default input file
+    if input is None:
+        data = __file__.replace('.py', '.input')
+        with open(data) as f:
+            input = [ trim(line) for line in f ]
+            # file is single line only
+            if len(input) == 1:
+                input = input[0]
+    #
+    print("TestCase B using input:", data if 'data' in vars() else input)
+    # read multiline string as input
+    if input.count('\n') > 2:
+        input = [ trim(line) for line in input.splitlines() ]
+        # optional delete the first empty line
+        if len(input[0]) == 0:
+            input = input[1:]
+    #
+    print("\t expected result:", result)
+    r = sut.task_b(input)
+    print('\t got:',r,'\t','[ OK ]' if r == result else '[ ERR ]')
+    print()
+
+
+# ======
+#  MAIN
+# ======
+
+print()
+print(__motd__, __url__)
+print()
+
+testdata = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+# ========
+#  Task A
+# ========
+
+# test cases
+testcase_a(CPU(), testdata,  13140)
+
+# 14860
+testcase_a(CPU(),   None,    14860)
+
